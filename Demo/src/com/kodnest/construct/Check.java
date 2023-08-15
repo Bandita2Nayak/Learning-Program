926944f0d98710b6c26fbcee9b871926d562a734
Bandita2Nayak <112819761+Bandita2Nayak@users.noreply.github.com>
2023-08-15 23:50:42 +0530

    VERSION 1
    Simple  Program

---
+++ Demo/src/com/kodnest/construct/Check.java
@@ -0,0 +1,9 @@
+package com.kodnest.construct;
+
+public class Check {
+	 void CheckEligibility(int marks )  {
+		if(marks>=80) {
+			 System.out.println("Welcome to Techclub"); 
+		 }
+	 }
+}
---
+++ Demo/src/com/kodnest/construct/Demo.java
@@ -0,0 +1,18 @@
+package com.kodnest.construct;
+
+import java.util.Scanner;
+
+public class Demo {
+	
+		 public static void main(String []args) {
+			 Scanner scan = new Scanner(System.in);
+			 System.out.println("Enter a number");
+			 int marks=scan.nextInt();
+			 System.out.println("Welcome to kodnest");
+			 Check check=new Check();
+			 check.CheckEligibility(marks);
+			 
+		 }
+		 
+		}
+
